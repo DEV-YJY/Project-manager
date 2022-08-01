bdf43a5899e7236a49e7e0387a02b9e6b6d271ee
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-01 18:44:45 +1200

    Create add Task button

--- components/BoardSection.tsx
+++ components/BoardSection.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import TaskComponent from './TaskComponent'
 import { Col, Button, Card, Form, Container } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 interface BoardSectionProps {
   title: String
@@ -14,7 +15,7 @@ const BoardSection: React.FC<BoardSectionProps> = ({ title, tasks }) => {
       <Col md={3} className='d-flex-column p-2'>
         <div className='board-section-header d-flex flex-row align-items-center'>
           <h3 className='me-auto'>{title}</h3>
-          <FontAwesomeIcon icon='fa-plus' style={{'color': '#6f7782'}} />
+          <FontAwesomeIcon icon={faPlus} style={{'color': '#6f7782'}} />
         </div>
         <Container className='p-0 d-flex flex-column h-100'>
           {tasks &&
@@ -28,6 +29,13 @@ const BoardSection: React.FC<BoardSectionProps> = ({ title, tasks }) => {
                 />
               )
             })
+          }
+          {
+            tasks.length > 0 &&
+            <Button className='add-wrapper'>
+              <FontAwesomeIcon icon={faPlus} style={{ 'padding': '2px' }} />
+              Add Task
+            </Button>
           }
         </Container>
       </Col>
