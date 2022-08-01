e04d4641409631f6ea98cd16d23c04e527900f14
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-01 16:49:39 +1200

    Render BoardSection within board component

--- pages/board.tsx
+++ pages/board.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { gql, useQuery } from '@apollo/client'
 import { Row } from 'react-bootstrap'
 import TaskComponent from '../components/TaskComponent'
+import BoardSection from '../components/BoardSection'
 
 const AllTasksQuery = gql `
   query {
@@ -20,24 +21,30 @@ function Board() {
       console.log(data)
     }
   })
+
+  const sections: string[] = ['Backlog', 'In-Progress', 'Review', 'Done']
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
   return (
-    <div className='pt-3 h-100 d-flex flex-column'>
-      <Row>
-        <h1>Project Title</h1>
-      </Row>
-      <div className='board-container d-flex flex-row flex-grow-1'>
-        {data && data.tasks.map((task: Task) => {
-          return ( 
-            <TaskComponent 
-              title={task.title}
-              description={task.description}
-              id={task.id}
-              key={task.id}
-            />
-          )
-        })}
+      <div className='pt-3 h-100 d-flex flex-column'>
+        <Row>
+          <h1>Project Title</h1>
+        </Row>
+        <div className='board-container d-flex flex-row flex-grow-1'>
+          {sections.map((section: String, index: number) => {
+            let filteredData: Array<Task> = data ? data.tasks.filter((task: Task) => {return task.status === section}) : []
+            return (
+              <BoardSection 
+                title='Backlog'
+                key={index}
+                tasks={filteredData}
+              />
+            )
+          })}
+        </div>
       </div>
-    </div>
   )
 }
 
