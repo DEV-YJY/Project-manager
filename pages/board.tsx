3c3f4598397517d55a1d8285c803974b90a009b2
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-04 13:40:21 +1200

    Allow dragging to change task status

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import { Row } from 'react-bootstrap'
 import TaskComponent from '../components/TaskComponent'
 import BoardSection from '../components/BoardSection'
@@ -16,6 +16,17 @@ const AllTasksQuery = gql `
   }
 `
 
+const UpdateTaskMutation = gql`
+  mutation UpdateTaskMutation($id: String!, $title: String, $description: String, $userId: String, $status: String!) {
+    updateTask(description: $description, id: $id, title: $title, userId: $userId, status: $status) {
+      id
+      title
+      description
+      status
+    }
+  }
+`
+
 function Board() {
   const { data, loading, error } = useQuery(AllTasksQuery, {
     onCompleted: data => {
@@ -24,10 +35,25 @@ function Board() {
   })
 
   const sections: string[] = ['Backlog', 'In-Progress', 'Review', 'Done']
+  const [updateTask] = useMutation(UpdateTaskMutation)
 
   const onDragEnd = (result) => {
     const { destination, source, draggableId } = result
-    console.log(result)
+
+    if (!destination) {
+      return
+    }
+
+    if (destination.droppableId === source.droppableId) {
+      return
+    }
+
+    updateTask({
+      variables: {
+        id: draggableId,
+        status: destination.droppableId
+      }
+    })
   }
 
   if (loading) return <p>Loading...</p>
