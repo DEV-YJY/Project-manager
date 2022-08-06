a25aac3a29a5c51555a3c205e27f37d133952cf1
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-06 14:51:56 +1200

    Update schema prisma to include sessions and account

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Card, Button, Form } from 'react-bootstrap'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/dist/client/router'
 
 const Login = () => {
   const { data: session } = useSession()
